b120cf41e85a8ef4cbbb51f56311a0c001548f0d
Ayusha Upreti <ayusha_upreti@hotmail.com>
2022-01-26 11:17:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { v4 } from 'uuid'
 import './App.css';
 
 function App() {
+
+  const [item, setItem] = useState('')
+  const [list, setList] = useState([])
+
+  const save = (e) => {
+    if(e.key === 'Enter'){
+      let l = list
+      l.push(e.target.value)
+      setList(l)
+      setItem("")
+    }
+  }
+
+  const deleteItem = (ind) => {
+    let l = list
+    l = l.filter((i, index) => index !== ind)
+    setList(l)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className='input-box' value={item} onChange={e => setItem(e.target.value)} onKeyPress={save}/>
+      <table className='table'>
+        <tbody>
+          {list && list.map((i, ind) => {
+            return <tr key={v4()} onClick={e => deleteItem(ind)}>
+              <td className='list-item'>{i}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
